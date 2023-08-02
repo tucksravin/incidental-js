0c27f3c3e5be28467b2364540ef924857dffc326
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2023-08-02 14:13:15 -0700

    working on desktop, not infinite

--- webflow/specific/beachfront/big-review.js
+++ webflow/specific/beachfront/big-review.js
@@ -1,5 +1,47 @@
+window.slider = {};
+window.slider.slides = $(".big-review-item");
+window.slider.i = 0;
+window.slider.length = $(".big-review-item").length;
+window.slider.inc = 20;
 
-//window.slider = $(".big-review-slider");
-//window.slider.css("bg-color", "red ");
+window.slider.checkInc = () => {
+if(window.innerWidth < 480){
+  window.slider.inc = 15;
+}
+else{
+    window.slider.inc = 20;
+}
 
-$(".review0slider-holder").css("background-color", "red");
+console.log(window.slider.inc);
+}
+
+window.slider.checkInc();
+window.onresize =  window.slider.checkInc;
+
+window.slider.next = () => {
+    window.slider.i++;
+    if(window.slider.i > window.slider.length-1){
+        window.slider.i = 0;
+    }
+
+    $(".big-review-slider").css("transform", "translateX(-" + window.slider.inc * window.slider.i + "rem)");
+}
+
+window.slider.prev = () => { 
+    window.slider.i--;
+    if(window.slider.i < 0){
+        window.slider.i = window.slider.length-1;
+    }
+
+    $(".big-review-slider").css("transform", "translateX(-" + window.slider.inc * window.slider.i + "rem)");
+}
+
+$(".big-review-arrow-right").click(window.slider.next);
+$(".big-review-arrow-left").click(window.slider.prev);
+
+//webflow specific bugs
+$(".big-review").css("opacity", "1");
+$(".big-review").css("transform", "translate3d(0, 0, 0)");
+
+
+
