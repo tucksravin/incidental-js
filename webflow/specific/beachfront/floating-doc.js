766d414ee8bfecf5ce98a2c1fa4162be07065549
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2023-08-07 15:18:30 -0700

    added resize listener to floating doc

--- webflow/specific/beachfront/floating-doc.js
+++ webflow/specific/beachfront/floating-doc.js
@@ -3,15 +3,22 @@ this.anchor = $(".ask-the-doctor-handwriting-anchor");
 this.allQuestions = $(".qa-block");
 this.currQuestion = 0;
 this.questionHeight = this.allQuestions.outerHeight(true);
-window.addEventListener("scroll", this.scrollingListener.bind(this));
 this.scrollingListener = () => {
-    if(this.anchor[0].getBoundingClientRect().top < 0){
+    if(this.anchor[0].getBoundingClientRect().top < 0 && this.currQuestion < this.allQuestions.length - 1){
         this.currQuestion++;
         this.anchor.css("transform", "translateY(" + this.questionHeight * this.currQuestion + "px)");
         }
-    else if(this.anchor[0].getBoundingClientRect().top > this.questionHeight){
+    else if(this.anchor[0].getBoundingClientRect().top > this.questionHeight && this.currQuestion > 0){
         this.currQuestion--;
         this.anchor.css("transform", "translateY(" + this.questionHeight * this.currQuestion + "px)");
         }   
-    }   
+    this.anchor.css("z-index", "5");
+    }
+    
+this.resizeListener = () => {
+    this.questionHeight = this.allQuestions.outerHeight(true);
+}
+    window.addEventListener("resize", this.resizeListener.bind(this));
+    window.addEventListener("scroll", this.scrollingListener.bind(this));
+    console.log("floating doctor loaded")
 }
