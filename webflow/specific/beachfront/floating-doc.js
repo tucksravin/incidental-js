40c26a11d193ea952f3a55db4c2a6c16d3469224
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2023-08-07 16:21:22 -0700

    swtiched from incrementing to a find method

--- webflow/specific/beachfront/floating-doc.js
+++ webflow/specific/beachfront/floating-doc.js
@@ -3,17 +3,33 @@ this.anchor = $(".ask-the-doctor-handwriting-anchor");
 this.allQuestions = $(".qa-block");
 this.currQuestion = 0;
 this.questionHeight = this.allQuestions.outerHeight(true);
+this.halfASecDelay = true;
 this.scrollingListener = () => {
-    if(this.anchor[0].getBoundingClientRect().top < 0 && this.currQuestion < this.allQuestions.length - 1){
-        this.currQuestion++;
+    if(!this.halfASecDelay) return;  
+
+    if((this.anchor[0].getBoundingClientRect().top < 0 && this.currQuestion < this.allQuestions.length - 1)||(this.anchor[0].getBoundingClientRect().top > this.questionHeight && this.currQuestion > 0)){
+        this.currQuestion = this.findTopVisibleQuestionIndex();
         this.anchor.css("transform", "translateY(" + this.questionHeight * this.currQuestion + "px)");
-        }
-    else if(this.anchor[0].getBoundingClientRect().top > this.questionHeight && this.currQuestion > 0){
-        this.currQuestion--;
-        this.anchor.css("transform", "translateY(" + this.questionHeight * this.currQuestion + "px)");
-        }   
-    this.anchor.css("z-index", "5");
+        this.anchor.css("z-index", "5");
+        
+
+
+    console.log(this.currQuestion);
+    this.halfASecDelay = false;
+    setTimeout(() => this.halfASecDelay = true , 500);
     }
+        
+
+    }
+this.findTopVisibleQuestionIndex = () => {
+    for(var i = 0; i < this.allQuestions.length; i++){
+        if(this.allQuestions[i].getBoundingClientRect().top > 0){
+             return i;
+            }
+        }
+    return this.allQuestions.length - 1;
+
+}
     
 this.resizeListener = () => {
     this.questionHeight = this.allQuestions.outerHeight(true);
